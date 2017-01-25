68554225049b7ed2d5426f2749b5128d8ddcc4da
Zachary Kirchin <zacharykirchin@gmail.com>
2017-01-25 15:47:25 -0500

    add speak method

--- pet.rb
+++ pet.rb
@@ -1,9 +1,15 @@
 class Pet
   attr_accessor :name, :type, :age
-  
+
   def initializer(name, type, age)
     @name = name
     @type = type
     @age = age
-   end
+  end
+
+  def speak
+    if @type == "dog"
+      return "woof"
+    end
+  end
 end
